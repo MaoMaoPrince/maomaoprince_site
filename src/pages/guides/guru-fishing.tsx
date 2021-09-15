56a242ef8cb869c124895524bb5543bdcbb91e5c
MaoMaoPrince <31109364+MaoMaoPrince@users.noreply.github.com>
2021-09-15 23:47:08 +0100

    Create guru-fishing.tsx

---
+++ src/pages/guides/guru-fishing.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { PageProps } from 'gatsby';
+
+import Title from '@/components/Title';
+
+const GuruFishingPage: React.FC<PageProps> = () => (
+  <main>
+    <Title />
+    <p> Guru Fishing Guide </p>
+      </main>
+);
+
+export default GuruFishingPage
